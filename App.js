56cc882c1f8ffd33e6bd1fbd24b849a5009d2ae9
Ridwan Balogun <balogunridwan@gmail.com>
2020-01-07 17:42:19 +0100

    convert to class component

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as tf from '@tensorflow/tfjs';
+import {fetch} from '@tensorflow/tfjs-react-native'
 
-export default function App() {
-  return (
+class App extends React.Component{
+  render(){
+   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
-  );
+  ); 
+  }
+  
 }
 
 const styles = StyleSheet.create({
@@ -18,3 +22,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+export default App;
